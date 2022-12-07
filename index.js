b0de2e9c6a2871b888f819877ce92ab3fd120aa5
Mohammed Diary Mohammed <hamadiary1@gmail.com>
2022-12-07 16:10:12 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(){
+    let i = 0;
+    let messages = [];
+    while (i < names.length){
+        messages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        i++;
+    }
+    return messages;
+}
+function countDown(){
+    let i = 10;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
